0dca5e5fb9dc64bae9aa85e70399676c8220b4d0
foultrip <davidvasquezmahecha@gmail.com>
2024-10-16 11:47:42 -0500

    fix: added in main file for change PORT deploy

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT'));
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
