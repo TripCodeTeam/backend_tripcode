4370229e3696fcc82cb9b265424fae4869563581
foultrip <davidvasquezmahecha@gmail.com>
2024-10-18 04:53:54 -0500

    feat: new property in schema db and file to deploy

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,8 @@
 export class CreateUserDto {
+  id?: string;
   email: string; // Email del usuario
   password: string; // Contraseña del usuario
   organizationName?: string; // Nombre de la organización (opcional)
+  created_at?: Date;
+  updated_at?: Date;
 }
